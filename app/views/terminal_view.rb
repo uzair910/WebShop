96166bc5e36389afe80411e2dbd492d98477fecb
uzair910 <UzairZ@jac.vpn>
2020-02-08 19:04:31 +0200

    terminal View added. not implmented yet

---
+++ app/views/terminal_view.rb
@@ -0,0 +1,9 @@
+require_relative "../controllers/welcome_terminal_controller.rb"
+
+class Inventory_Model
+  def Init()
+    puts "welcome to terminal edition (phase 2)"
+  end
+end
+
+Inventory_Model.new.Init
